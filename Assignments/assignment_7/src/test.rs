bef885c94e1534eea9dcffb06c4a0aab80221c51
ajstyles0524 <71371404+ajstyles0524@users.noreply.github.com>
2021-10-25 21:03:28 +0530

    adding seventh-assignment

--- Assignments/assignment_7/src/test.rs
+++ Assignments/assignment_7/src/test.rs
@@ -2,23 +2,23 @@
 mod tests {
     use crate::error_handling::check_even_number::response_handle;
     #[test]
-    fn check_even_success_first(){
+    fn check_even_success_first() {
         assert_eq!(response_handle(98), "Even Number".to_string());
     }
     #[test]
-    fn check_even_success_second(){
-        assert_eq!(response_handle(0), "Even Number".to_string());
+    fn check_even_success_second() {
+        assert_eq!(response_handle(14), "Even Number".to_string());
     }
     #[test]
-    fn check_even_failure_first(){
-        assert_ne!(
+    fn check_even_failure_first() {
+        assert_eq!(
             response_handle(3),
             "Please provide correct input".to_string()
         );
     }
     #[test]
-    fn check_even_failure_second(){
-        assert_ne!(
+    fn check_even_failure_second() {
+        assert_eq!(
             response_handle(7),
             "Please provide correct input".to_string()
         );
