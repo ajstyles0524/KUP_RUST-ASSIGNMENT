cb69d8f02581a1ffb90bd5a74e206c27f701d85f
ajstyles0524 <ajstyles0524@gmail.com>
2021-11-22 13:19:18 +0530

    added tenth-assignment

---
+++ Assignments/assignment-10/src/task/first_repeated.rs
@@ -0,0 +1,26 @@
+use log::*;
+use crate::list::List;
+use crate::list::List::{Cons, Nil};
+
+/// first_consecutive_repeated function is used to find first repeating element in list
+///
+/// #Argument
+///
+/// temporary_number - An i32 type variable containing the previous value in Cons tuple of List enum.Take default -1
+/// list - A List of enum type containing Box<i32> and Nil.
+///
+/// #Return
+///
+/// Return Result<i32,String>,first repeating element from a given list and handle error as well.
+pub fn first_consecutive_repeated(temporary_number: i32, list: List) -> Result<i32, String> {
+    match list {
+        Cons(current_number, list) => match temporary_number == current_number {
+            true => Ok(current_number),
+            false => first_consecutive_repeated(current_number, *list),
+        },
+        Nil => {
+            error!("Empty box provided in the given list");
+            Err("Please Provide valid list".to_string())
+        }
+    }
+}
---
+++ Assignments/assignment-10/src/task/second_repeated.rs
@@ -0,0 +1,36 @@
+use log::*;
+use crate::list::List;
+use crate::list::List::{Cons, Nil};
+
+/// second_consecutive_repeated function is used to find second repeating element in list
+///
+/// #Argument
+///
+/// temporary_number - An i32 type variable containing the previous value in Cons tuple of List enum.Take default -1
+/// count - counter of i32 type integer
+/// list - A List of enum type containing Box<i32> and Nil.
+///
+/// #Return
+///
+/// Return Result<i32,String>,second repeating element from a given list and handle error as well.
+pub fn second_consecutive_repeated(
+    temporary_number: i32,
+    count: i32,
+    list: List,
+) -> Result<i32, String> {
+    match list {
+        Cons(current_number, list) => {
+            if temporary_number == current_number && count == 0 {
+                second_consecutive_repeated(current_number, count + 1, *list)
+            } else if temporary_number == current_number && count == 1 {
+                Ok(current_number)
+            } else {
+                second_consecutive_repeated(current_number, count, *list)
+            }
+        }
+        Nil => {
+            error!("Empty box provided in the given list");
+            Err("Please Provide valid list".to_string())
+        }
+    }
+}
